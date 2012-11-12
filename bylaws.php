1636a1eddc9f01ced3c87c79b57d17122704ac5d
middle8media <seth@middle8media.com>
2012-11-12 16:41:14 -0500

    added bylaws.php

---
+++ bylaws.php
@@ -0,0 +1,111 @@
+<!doctype html>
+<html>
+<head>
+	<meta charset="utf-8" />
+	<title>TIFN - Triad Indie Film Network</title>
+
+	<link rel="stylesheet" href="css/960.min.css">
+	<link rel="stylesheet" href="css/style.css">
+
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+	<script src="/js/scrollspy.js"></script>
+
+</head>
+
+
+<body data-spy="scroll">
+
+<div class="container_12">
+	<div id="header" class="grid_12">
+		<a href="http://www.facebook.com/TriadIndie" title="TIFN on Facebook">
+			<div id="facebook" class="grid_1 alpha"></div>
+		</a>
+		<div id="logo" class="grid_10">
+			<h1><a href="#welcome" title="TRIAD INDIE FILM NETWORK">TRIAD INDIE FILM NETWORK</a></h1>
+		</div>
+		<a href="http://twitter.com/TriadIndieFilm" title="TIFN on Twitter">
+			<div id="twitter" class="grid_1 omega"></div>
+		</a>
+		<nav id="navbar" class="grid_12">
+			<ul>
+				<li><a href="/#welcome" title="Welcome">Welcome</a></li>
+				<li><a href="/#about" title="About">About</a></li>
+				<li><a href="/#membership" title="Membership">Membership</a></li>
+				<li><a href="/#events" title="Events">Events</a></li>
+				<li><a href="/#links" title="Links">Links</a></li>
+				<li><a href="/#contact" title="Contact">Contact</a></li>
+			</ul>
+		</nav>
+	</div>
+</div>
+
+
+<div class="container_12">
+
+<div id="bylaws" class="grid_12">
+	<div class="content-container">
+		<h2>From the Bylaws:</h2>
+		<div class="grid_6 alpha">
+			<h3>Officers:</h3>
+				<ul>
+					<h4>Section 1:</h4>
+					<li>Officers must be current members in good standing.</li><br />
+					<h4>Section 2 - Officer Titles:</h4>
+					<li>President, Vice President, Secretary, Treasurer, Marketing Guru, Web Guru.</li><br />
+					<h4>Section 3 - Officer Roles:</h4>
+						<ul>
+							<h4>President:</h4>
+							<li>oversees meetings; acts as liaison with outside entities; sets agendas.</li><br />
+							<h4>Vice President:</h4>
+							<li>supports president, including stepping in to that role when need arises; oversees membership; updates bylaws.</li><br />
+							<h4>Secretary:</h4>
+							<li>keeps minutes at meetings; disburses information to officers.</li><br />
+							<h4>Treasurer:</h4>
+							<li>oversees funds and bank account.</li><br />
+							<h4>Marketing Guru:</h4>
+							<li>advertises for each meeting and promotes the TIFN name.</li><br />
+							<h4>Web Master:</h4>
+							<li>maintains TIFN website and social networking sites.</li>
+						</ul><br />
+					<h4>Section 4:</h4>
+					<li>Term is for 1 year.</li><br />
+					<h4>Section 5:</h4>
+					<li>Officers can be removed from posts with a 2/3 majority vote of officers called at any time.</li>
+				</ul>
+
+		</div>
+		<div class="grid_6 omega">
+			<h3>Article V - Elections:</h3>
+				<ul>
+					<h4>Section 1:</h4>
+					<li>Each officer is elected to a one-year term. There is no limit to years in office.</li><br />
+					<h4>Section 2:</h4>
+					<li>Officers are elected by members who can vote (see above).</li><br />
+					<h4>Section 3:</h4>
+					<li>Any current member can run for office. Simple majority wins. Elections are held in the first week of December. In the event of a tie a revote will be held between the two tied candidates. Re-votes will continue until the tie is broken.</li><br />
+					<h4>Section 4:</h4>
+					<li>Nominations will be solicited 2 weeks before the election. If at least one person per office is not nominated, outgoing officers will put forth nominations.</li>
+				</ul>
+		</div>
+	</div>
+</div>
+
+<div class="grid_12" style="margin-bottom: 100px;">
+
+</div>
+
+</div>
+	<script type="text/javascript">
+		$('nav li a').click(function()
+		{
+		  $('nav li').removeClass('active');
+		  $(this).parent().addClass('active');
+		});
+	</script>
+
+	<script>
+	  $('#navbar').scrollspy()
+	</script>
+
+</body>
+</html>
