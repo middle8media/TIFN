948d2bf6c859439b6caa50f2d401cc7d444caf7c
middle8media <seth@middle8media.com>
2012-11-14 20:44:44 -0500

    fixed site structure - added fruitcake folder

---
+++ fruitcake/index.php
@@ -0,0 +1,140 @@
+---
+layout: default
+---
+
+<body>
+
+<div id="christmas-lights"></div>
+
+<div id="header">
+	<div class="container">
+		<div class="sixteen columns">
+			<h1 class="logo"><a href="/">{{site.site_name}}</a></h1>
+			<div class="tagline">{{site.tagline}}</div>
+		</div> <!-- sixteen columns -->
+	</div> <!-- container -->
+</div> <!-- #header -->
+
+<div id="fruitcake-img">
+	<div class="container">
+		<div class="fruitcake-img sixteen columns">
+			<img src="/img/fruit-cake-banner.jpg" alt="Fruit Cake">
+			<p class="film-challenge">The 2012 Fruitcake Film Challenge Film Festival</p>
+		</div> <!-- sixteen columns -->
+	</div> <!-- container -->
+</div> <!-- #fruitcake-img -->
+
+<div id="details">
+	<div class="container">
+		<div class="sixteen columns">
+			<a class="down-please" href="#rules" title="Down Please">&dArr;</a>
+			<h2>Details</h2>
+				<div class="row">
+					<div class="eight columns alpha">
+					<h3>What?</h3>
+						<p>A fun holiday filmmaking challenge aimed at both beginner/first time filmmakers (including kids) and experienced filmmakers to create 10 second, 30 second and 60 second categories. Your film will screen on the big screen in real movie theater (at the Carousel Luxury Cinemas).</p>
+					</div>
+					<div class="eight columns omega offset-by-one">
+					<h3>When & Where</h3>
+						<table>
+						<tr>
+							<td><strong>When:</strong></td>
+							<td>Saturday, December, 1st @ 1pm</td>
+						</tr>
+						<tr>
+							<td><strong>Where:</strong></td>
+							<td>The Creative Center - <a href="http://g.co/maps/kkcx5" target="blank" title="View Map">View Map</a><br></td>
+						</tr>
+					</table>
+					</div>
+				</div> <!-- row -->
+				<div class="sixteen columns alpha blue-box">
+					<p>Join us on <a>Saturday, December 1st at 1:00 p.m. at the Creative Center</a> for a FREE workshop, as we make holiday themed movies in just 2 hours! This festival offers a wonderful opportunity for first time film makers and the most seasoned veterans to network, learn, and share in the film creation process. Participants will have 2 hours to conceive, shoot and finish a holiday-themed short film.</p>
+					<p>How short? you may ask. Well that is your choice of 10 seconds or 30 seconds or 60 seconds. Yes, you can do this in two hours, but it is a creative film challenge!</p>
+					<p>This is a FREE TIFN event, open to the public, and EVERYONE can enter. You can bring your own team, work solo, or come and be part of a team at the event. All you need is a video camera, be it a cell phone or full pro gear, or only a computer with animation software, and most important, the desire to make a short film very quickly. TIFN officers and members will be available to help answer questions and provide how-to tips and tricks throughout the event.</p>
+					<br />
+				</div> <!-- sixteen columns -->
+		</div> <!-- sixteen columns -->
+	</div> <!-- container -->
+</div> <!-- #details -->
+
+<div id="rules">
+	<div class="container">
+		<div class="sixteen columns">
+		<a class="down-please" href="#sponsors" title="Down Please">&dArr;</a>
+		<a class="up-please" href="#details" title="Up Please">&uArr;</a>
+		<h2>Rules & Guidelines</h2>
+			<div class="row">
+				<div class="six columns alpha">
+				<h3>Festival Rules</h3>
+					<ol>
+						<li>Films should be 10-second, 30-second, or 60-second in length</li>
+						<li>You may submit more than one film</li>
+						<li>Each film must adhere to one of the holiday-based themes to be revealed on December 1st</li>
+						<li>This is a holiday film festival, so please, PG rated themes only</li>
+						<li>TIFN reserves the right to refuse screening of any film based on content</li>
+						<li>If you are unable to join us on Sat December 1st, you are still welcome to make and submit film(s)</li>
+						<li>ALL entries must be received by 11:59 p.m. on Monday, December 3rd.</li>
+					</ol>
+				</div>
+				<div class="ten columns omega offset-by-one">
+					<h3>Submission Guidelines</h3>
+					<ol>
+						<li>Films can be created with any camera and editing program</li>
+						<li>As long as your file plays on a Mac or PC computer or your smartphone or tablet, we can screen it</li>
+						<li>For best results on the big screen, create the film as the highest resolution possible e.g. “HD or 1080p”</li>
+						<li>Your audio needs to be clear – not too loud, not too soft</li>
+						<li>For the technical geeks and/or the professional filmmakers – a 24 frames per second, 1080p file (e.g. ProRes or Avid 4:2:2) will look best</li>
+						<li>For the wannabee Kubricks out there, a 2K or 4K master in Flat or Scope aspect ratio with 5.1 surround will play best</li>
+						<li>If you don’t know what any of this means, don’t worry about it, just make your film and have fun doing it!</li>
+					</ol>
+					<br />
+				</div>
+			</div> <!-- row -->
+		</div> <!-- sixteen columns -->
+	</div> <!-- container -->
+</div> <!-- #details -->
+
+<div id="sponsors">
+	<div class="container">
+		<div class="sixteen columns">
+			<a class="up-please" href="#header" title="Top Please"><span>Top Please</span> &uArr;</a>
+			<h2>Sponsors</h2>
+			<div class="row">
+				<div class="six columns alpha">
+				<h3>Our Sponsors</h3>
+					<ul>
+						<li><a href="http://triadindie.org" target="_blank" >TIFN</a></li>
+						<li><a href="http://www.thecreativecenter.net/" target="_blank" >The Creative Center</a></li>
+						<li><a href="http://www.carouselbattleground.com/" target="_blank" >The Carousel Theater</a></li>
+						<li><a href="http://www.sv2studios.com/" target="_blank" >SV2 Studios</a></li>
+						<li><a href="http://middle8media.com/" target="_blank" >Middle8Media</a></li>
+						<li><a href="http://idiotboxers.com/" target="_blank" >The Idiot Box</a></li>
+						<li><a href="http://springgardenbakery.com/" target="_blank" >Spring Garden Bakery</a></li>
+						<li><a href="http://www.facebook.com/pages/Donut-World-Greensboro/114396681972309" target="_blank" >Donut World</a></li>
+						<li><a href="http://www.theporterhousebarandgrill.com/" target="_blank" >The Porterhouse</a></li>
+					</ul>
+					<br />
+				</div>
+				<div class="ten columns omega offset-by-one">
+					<h3>Screening Info</h3>
+						<table>
+							<tr>
+								<td><strong>When:</strong></td>
+								<td>Wednesday, December 12th @ 7pm</td>
+							</tr>
+							<tr>
+								<td><strong>Where:</strong></td>
+								<td>The Carousel Theater - <a href="http://goo.gl/maps/Q70rY" target="blank" title="View Map">View Map</a><br></td>
+							</tr>
+						</table>
+						<div class="green-box">
+							<p>Tickets for the event are only $5.00, so you can invite all your friends and family to share in this entertaining holiday film event.</p>
+							<p>Exciting prizes will be awarded based on audience votes!</p>
+						</div>
+				</div>
+			</div> <!-- row -->
+		</div> <!-- sixteen columns -->
+	</div> <!-- container -->
+</div> 	<!-- #sponsors -->
+
