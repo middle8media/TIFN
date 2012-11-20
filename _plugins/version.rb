cbdccdf32adabd10c2ceae45ebecb6a14a1b76fa
middle8media <seth@middle8media.com>
2012-11-20 11:15:58 -0500

    added github.php post receive hook

--- _plugins/version.rb
+++ _plugins/version.rb
@@ -1,7 +1,7 @@
 module Jekyll
   module VersionFilter
     def versioned_url(input)
-      "http://localhost:8888/css/global.css#{input}?#{Time.now.to_i}"
+      "#{input}?#{Time.now.to_i}"
     end
   end
 end
